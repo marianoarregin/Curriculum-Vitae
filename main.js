e82a5670a7d062d9b4c4e9059f6d24eafe7ed5f8
Mariano Romero Arregin <marianoarregin@gmail.com>
2023-01-20 15:21:59 -0300

    JS - Efecto ScrollReveal

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.main');
+ScrollReveal().reveal('.experiencia', { delay: 500 });
+ScrollReveal().reveal('.formacion', { delay: 500 });
+ScrollReveal().reveal('.habilidades', { delay: 500 });
