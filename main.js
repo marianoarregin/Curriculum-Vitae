22dbf74d2658a82fd5392d8bc0b4b298fb778220
Mariano Romero Arregin <marianoarregin@gmail.com>
2023-01-22 13:23:45 -0300

    HTML - CSS - JS: modo oscuro

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ ScrollReveal().reveal('.main');
 ScrollReveal().reveal('.experiencia', { delay: 500 });
 ScrollReveal().reveal('.formacion', { delay: 500 });
 ScrollReveal().reveal('.habilidades', { delay: 500 });
+
+let tema=document.getElementById("tema");
+let body=document.body;
+
+tema.addEventListener("click", function(){
+    let val=body.classList.toggle("dark")
+    localStorage.setItem("tema",val)
+})
+
+let valor=localStorage.getItem("tema")
+
+if (valor=="true") {
+    body.classList.add("dark")
+} else {
+    body.classList.remove("dark")
+}
